33931650617a011ad34ae5213e0059f890c64752
Kokemusi <74450392+Kokemusi@users.noreply.github.com>
2024-02-13 22:19:28 +0900

    Update simulator.js

--- simulator.js
+++ simulator.js
@@ -1,3 +1,6 @@
+//変数宣言
+let element_units = {cryo:0,dendro:0,pyro:0,burning:0};
+//関数定義
 function ganyu(distance){
   let data = {
     CA:{
@@ -12,6 +15,7 @@ function ganyu(distance){
   };
   return data;
 }
+
 function nahida(){
   let data = {
     tE:{
@@ -36,6 +40,7 @@ function nahida(){
   };
   return data;
 }
+
 function bennett(){
   let data = {
     tE:{
@@ -57,7 +62,7 @@ function bennett(){
   };
   return data;
 }
-console.log(ganyu(2));
+
 function make_plan(rotation){
   let char_data = {Ganyu:ganyu(0),
                    Nahida:nahida(),
@@ -78,14 +83,74 @@ function make_plan(rotation){
       }
     }else{
       for(let j = 1; j<reading[rotation[i]]["hit_num"]+1; j++){
-        console.log(reading[rotation[i]]["hit"+j]);
         plan["_"+(action_frame+reading[rotation[i]]["hit"+j])]={type:reading[rotation[i]]["type"],GU:reading[rotation[i]]["GU"],ICD:reading[rotation[i]]["ICD"]};
       }
-      console.log(reading[rotation[i]]);
       action_frame += reading[rotation[i]]["frame"][rotation[i+1]];
     }
-    console.log(action_frame,i,rotation[i]);
   }
   console.log(plan);
+  return plan;
 }
-make_plan(["swap","Nahida","tE","Q","swap","Bennett","tE","Q","swap","Ganyu","CA","CA","CA","CA","CA","CA","CA","swap"]);
+
+function simulate(plan){
+  let TKP_CD = 0;
+  let burning_pyro = 0;
+  let plan_key=Object.keys(plan);
+  for(let frame = 0; frame<1100; frame++){
+    if(plan_key.includes("_"+frame){
+     let reaction = applicate(plan["_"+frame]);
+     if(!(reaction="none")){
+       if(TKP_CD==0){
+         TKP_CD = 2.5*60;
+         plan_key["_"+(frame+4)]={type:"dendro",GU:1.5,ICD:1};
+       }
+     }
+    }
+    console.log(reaction);
+    if(element_units.burning>0){
+      if(burning_pyro == 120){
+        element_units.pyro = 1;
+      }else{
+        burning_pyro++;
+      }
+    }
+    if(TKP_CD>0){
+      TKP_CD--;
+    }
+  }
+}
+function applicate(element_data){
+  let type_ = element_data.type;
+  let units_ = element_data.GU;
+  if(type_ == "pyro"){
+    if(element_units.pyro<units_){
+      element_units.pyro = 0.8*units_;
+    }
+  }
+  if(type_ == "cryo"){
+    if(element_units.burning>0){
+      element_units.burning -= 0.5*units_;
+      if(element_units.burning<0){
+        element_units.burning = 0;
+        return "melt";
+      }
+    }else if(element_units.pyro){
+      element_units.pyro -= 0.5*units_;
+      if(element_units.pyro<0){
+        element_units.pyro = 0;
+        return "melt";
+      }
+    }else{
+      element_units.cryo = 0.8*units_;
+    }
+  }
+  if(type_ == "dendro"){
+    element_units.dendro = 0.8*units_;
+    if(element_units.pyro>0 && element_units.burning==0){
+      element_units.burning = 2;
+      return "burning";
+    }
+  }
+  return "none";
+}
+simulate(make_plan(["swap","Nahida","tE","Q","swap","Bennett","tE","Q","swap","Ganyu","CA","CA","CA","CA","CA","CA","CA","swap"]));
