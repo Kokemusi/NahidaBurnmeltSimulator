6769ec98744ea98a728e1f938dcc7d15bc0c8e61
Kokemusi <74450392+Kokemusi@users.noreply.github.com>
2024-02-13 17:03:39 +0900

    Update simulator.js

--- simulator.js
+++ simulator.js
@@ -11,10 +11,10 @@ function ganyu(distance){
     }
   };
   return data;
-};
+}
 function nahida(){
   let data = {
-    E:{
+    tE:{
       frame:{Q:32,swap:25},
       hit_num:1,
       hit1:13,
@@ -35,5 +35,48 @@ function nahida(){
       ICD:1,}
   };
   return data;
-};
+}
+function bennett(){
+  let data = {
+    tE:{
+      frame:{Q:51,swap:50},
+      hit_num:1,
+      hit1:16,
+      type:"pyro",
+      GU:2,
+      ICD:0,
+    },
+    Q:{
+      frame:{E:52,swap:51},
+      hit_num:1,
+      hit1:37,
+      type:"pyro",
+      GU:2,
+      ICD:0,
+    }
+  };
+}
 console.log(ganyu(2));
+function make_plan(rotation){
+  let char_data = {Ganyu:ganyu(),
+                   Nahida:nahida(),
+                   Bennett:bennett()
+                  };
+  let char_field = "Nahida";
+  let action_frame = 0;
+  let reading = char_data[char_field];
+  let plan = {};
+  console.log(char_data.keys());
+  for(let i=0; i<rotation.length;i++){
+    if(rotation[i]="swap"){
+      i++;
+      if(!(char_field=rotation[i])){
+        reading = char_data[char_field];
+        action_frame += 3;
+      }
+    }else{
+      reading[rotation[i]];
+    }
+  }
+}
+make_plan(["swap","Nahida","tE","Q","swap","Bennett","tE","Q","swap","Ganyu","CA","CA","CA","CA","CA","CA","CA"]);
